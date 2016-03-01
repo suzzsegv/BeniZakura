4b11cf0b52fda08f443ddf22b821f7ce749076a9
Suzuki Satoshi <suzzsegv@gmail.com>
2016-03-01 19:18:12 +0900

    バージョンを 2016.3.1.29 に変更
    Copyright に 2016 を追加

--- sakura_core/dlg/CDlgAbout.cpp
+++ sakura_core/dlg/CDlgAbout.cpp
@@ -178,7 +178,7 @@ BOOL CDlgAbout::OnInitDialog( HWND hwndDlg, WPARAM wParam, LPARAM lParam )
 	auto_sprintf( szMsg, L"      %s  %s   Built with %s\r\n", _T(__DATE__), _T(__TIME__), vcVersion );
 	cmemMsg.AppendString( szMsg );
 	cmemMsg.AppendString( _T("\r\n") );
-	cmemMsg.AppendString( _T("      Copyright (C) 2012, 2013, 2014, 2015  by Satoshi Suzuki\r\n") );
+	cmemMsg.AppendString( _T("      Copyright (C) 2012, 2013, 2014, 2015, 2016 by Satoshi Suzuki\r\n") );
 	cmemMsg.AppendString( _T("\r\n") );
 	cmemMsg.AppendString( _T("\r\n") );
 
