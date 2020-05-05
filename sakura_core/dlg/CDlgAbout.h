97c455a407ec41cadefe36f878da658b477ce3c5
Satoshi Suzuki <suzzsegv@gmail.com>
2020-05-05 12:19:47 +0900

    About ダイアログ: 未使用の処理を削除

--- sakura_core/dlg/CDlgAbout.h
+++ sakura_core/dlg/CDlgAbout.h
@@ -23,23 +23,6 @@
 	DispatchEventを独自に定義することで，CDialogでサポートされていない
 	メッセージを捕捉する．
 */
-
-class CUrlWnd
-{
-public:
-	CUrlWnd() { m_hWnd = NULL; m_hFont = NULL; m_bHilighted = FALSE; m_pOldProc = NULL; }
-	virtual ~CUrlWnd() { ; }
-	BOOL SetSubclassWindow( HWND hWnd );
-	HWND GetHwnd() const{ return m_hWnd; }
-protected:
-	static LRESULT CALLBACK UrlWndProc( HWND hWnd, UINT msg, WPARAM wp, LPARAM lp );
-protected:
-	HWND m_hWnd;
-	HFONT m_hFont;
-	BOOL m_bHilighted;
-	WNDPROC m_pOldProc;
-};
-
 class CDlgAbout : public CDialog
 {
 public:
@@ -49,10 +32,6 @@ public:
 protected:
 	BOOL OnInitDialog( HWND, WPARAM, LPARAM );
 	BOOL OnBnClicked( int );
-	BOOL OnStnClicked( int );
-private:
-	CUrlWnd m_UrlUrWnd;
-	CUrlWnd m_UrlOrgWnd;
 };
 
 ///////////////////////////////////////////////////////////////////////
